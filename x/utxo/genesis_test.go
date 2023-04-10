e4bea748373c677778336356df45b9da94969eb2
satellitex <s.a.t.e.3.ths@gmail.com>
2023-04-11 00:19:37 +0400

    add genesis state

--- x/utxo/genesis_test.go
+++ x/utxo/genesis_test.go
@@ -3,27 +3,45 @@ package utxo_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	keepertest "utxo/testutil/keeper"
 	"utxo/testutil/nullify"
 	"utxo/x/utxo"
 	"utxo/x/utxo/types"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGenesis(t *testing.T) {
+	// Test addresses
+	addresses := []string{
+		"address1",
+		"address2",
+		"address3",
+		"address4",
+		"address5",
+	}
+
+	// Create initial UTXOs
+	var utxoList []*types.TxOut
+	for _, addr := range addresses {
+		utxoList = append(utxoList, &types.TxOut{
+			PkScript: addr,
+			Value:    10000,
+		})
+	}
+
+	// Create initial transaction with all UTXOs in a single txOut
+	transaction := types.Transaction{
+		Version:  1,
+		TxOut:    utxoList,
+		Locktime: 0,
+	}
+
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
 
-		TransactionList: []types.Transaction{
-			{
-				Id: 0,
-			},
-			{
-				Id: 1,
-			},
-		},
-		TransactionCount: 2,
-		// this line is used by starport scaffolding # genesis/test/state
+		TransactionList:  []types.Transaction{transaction},
+		TransactionCount: 1,
 	}
 
 	k, ctx := keepertest.UtxoKeeper(t)
